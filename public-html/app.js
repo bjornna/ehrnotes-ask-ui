11f4c908c4bac8d3d3b329d77b07a7d8c07f9cc2
Bjørn Næss <bna@dips.no>
2020-02-06 23:38:57 +0100

    Update on app to make it more robust. Added bootswatch to make it look like a prototype

--- public-html/app.js
+++ public-html/app.js
@@ -1,30 +1,44 @@
+
 var app = new Vue({
     el: "#app",
     data() {
         return {
             info: null,
             counter: 0,
+            debouncedInput: null,
             status: [],
             corpus: null,
             //host: "http://localhost:8000/entview",
             host: "https://ehrnotes-ask.azurewebsites.net/entview",
+            debug: false,
             loading: false,
             errored: false,
             show_tip: false
         };
     },
+    watch: {
+
+        corpus: debounce(function (newVal) {
+            this.debouncedInput = newVal;
+            this.nlp(this.corpus,null);
+        },1000)
+    },
     methods: {
+        doNLP: function () {
+            this.nlp(this.corpus,null);
+        },
         update_status: function (message) {
-            this.loading = true;
+
             console.log("NEW STATUS -> " + message);
             this.status.push(message);
         },
         nlp: function (message,event) {
             self = this;
+            self.loading = true;
             self.update_status("NLP inveoked");
             axios
                 .post(self.host,{
-                    corpus: self.corpus
+                    corpus: message
                 })
                 .then(function (response) {
                     self.update_status("Got NLP response");
